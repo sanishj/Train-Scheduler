6ddc0921f6f6b6fe584c093fe788a628298aa0f4
sanishjohn <sanishj@gmail.com>
2017-06-23 17:28:09 -0500

    incremental changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,44 @@
 $(document).ready(function() {
-
-
-    // <script src="https://www.gstatic.com/firebasejs/4.1.2/firebase.js"></script>
-
     // Initialize Firebase
-      var config = {
+    var config = {
         apiKey: "AIzaSyBxMFppZPw_ZHM_9Dh-VJvhkMrezTShtsM",
         authDomain: "traintrkr.firebaseapp.com",
         databaseURL: "https://traintrkr.firebaseio.com",
         projectId: "traintrkr",
         storageBucket: "traintrkr.appspot.com",
         messagingSenderId: "1087400696597"
-      };
-      firebase.initializeApp(config);
+    };
+
+    firebase.initializeApp(config);
 
     var database = firebase.database();
+
+    // Button event controller
+    $("#js-AddTrain").on("click", function(event) {
+        // prevent form from submitting
+        event.preventDefault();
+        var trainName = $("#js-TrainName").val().trim();
+        var destination = $("#js-Dest").val().trim();
+        var firstTrain = $("#js-FirstTrain").val().trim();
+        var frequency = $("#js-Frequency").val().trim();
+        
+        var newTrain = {
+            train: trainName,
+            destination : destination,
+            firstTrain : firstTrain,
+            frequency : frequency
+        };
+
+        console.log(newTrain);
+        
+
+
+        database.ref().push({
+            TrainName: trainName,
+            TrainDestination: destination,
+            FirstTrain: startDate,
+            TrainFrequency: monthlyRate
+        });
 
     var name = "";
     var role = "";
@@ -23,27 +47,12 @@ $(document).ready(function() {
     var monthlyRate = "";
     var totBilled = "";
 
-    $("#addEmp").on("click", function(event) {
-        // prevent form from submitting
-        event.preventDefault();
-        trainName = $("#js-TrainName").val().trim();
-        destination = $("#js-Dest").val().trim();
-        firstTrain = $("#js-FirstTrain").val().trim();
-        frequency = $("#js-Frequency").val().trim();
-        console.log(trainName);
-        database.ref().push({
-            TrainName: trainName,
-            TrainDestination: destination,
-            FirstTrain: startDate,
-            TrainFrequency: monthlyRate
-        });
         /*var dynamicEmpRow = $("<tbody>" + "<tr>" + "<td>" + name +"</td>" + "</tr>" + "</tbody>");
         var dynamicRoleRow = $("<tbody>" + "<tr>" + "<td>" + role +"</td>" + "</tr>" + "</tbody>");
         var dynamicDateRow = $("<tbody>" + "<tr>" + "<td>" + startDate +"</td>" + "</tr>" + "</tbody>");
         var dynamicRateRow = $("<tbody>" + "<tr>" + "<td>" + monthlyRate +"</td>" + "</tr>" + "</tbody>");*/
-        var dynamicRow = $("<tbody>" + "<tr>" + "<td>" + trainName +"</td>" + "<td>" + destination +"</td>" 
-            + "<td>" + firstTrain +"</td>" + monthsWorked +"</td>" + "<td>" + frequency +"</td>" + 
-            "<td>" + totBilled +"</td>"+ "</tr>" + "</tbody>");
+        var dynamicRow = $("<tbody>" + "<tr>" + "<td>" + trainName + "</td>" + "<td>" + destination + "</td>" + "<td>" + firstTrain + "</td>" + monthsWorked + "</td>" + "<td>" + frequency + "</td>" +
+            "<td>" + totBilled + "</td>" + "</tr>" + "</tbody>");
         /*$("#emp").append(dynamicEmpRow);
         $("#roleId").append(dynamicRoleRow);
         $("#dateId").append(dynamicDateRow);
